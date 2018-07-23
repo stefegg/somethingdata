6887291f8cf61ab8e7e4acfb7364f902f30f26c1
Stef Egbert <stephenegbert@gmail.com>
2018-07-23 15:23:52 -0400

    lol

---
+++ application.js
@@ -0,0 +1,20 @@
+// myBows = [];
+weaponStuff = [];
+
+// function getBow() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myBows = JSON.parse(this.responseText);
+      }
+    };
+  xhttp.open("GET", "https://raw.githubusercontent.com/stefegg/somethingdata/master/bows.json", true);
+  xhttp.send();
+// }
+
+
+
+function showWeapon(id){
+  document.getElementbyId('waeponName').innerHTML = allBows[id].weapons.bows.name;
+
+}
